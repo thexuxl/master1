f5134255f5746385665a20d9b65823726282be51
许晓良 <xuxiaoliang@xuxiaolangdeMBP.lan>
2023-02-08 13:46:33 +0800

    webpack--build

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,35 +1,44 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
-    entry: path.join(_dirname, "../src/index.tsx"),//入口文件
+    entry: path.join(__dirname, "../src/index.tsx"),//入口文件
     output: {
         filename: "static/js/[name].js",//每个输出js的名称
-        path: path.join(_dirname, "../dist"),//打包输出路径
+        path: path.join(__dirname, "../dist"),//打包输出路径
         // clean: true,//webpack4需要，webpack5就内置了
         publicPath: "/"
     },
     //rules
-    rules: [
-        {
-            test: /.(ts|tsx)$/,//匹配ts。tsx文件
-            use: {
-                loader: "babel-loader",
-                options: ["@babel/preset-react", "@babel/preset-typescript"]
+    module: {
+        rules: [
+            {
+                test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+                        presets: [
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
+                        ]
+                    }
+                }
             }
+        ]
+    },
 
-        },
 
-
-
-    ],
     //resolve
     resolve: {
-        extensions: [".jsx", ".js", ".tsx", ".ts"]
+        extensions: [".jsx",'.js', '.tsx', '.ts'],
     },
     //plugin
     plugins: [
-
-
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
+            inject: true, // 自动注入静态资源
+        })
     ]
+
 
 }
